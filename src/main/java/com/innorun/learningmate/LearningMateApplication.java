401cbf0386da2272774aca27307ddcf3e2cc2398
Sooyoung Yoon <ayoooo240@gmail.com>
2026-08-19 12:47:16 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/innorun/learningmate/LearningMateApplication.java
@@ -0,0 +1,13 @@
+package com.innorun.learningmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningMateApplication.class, args);
+    }
+
+}
