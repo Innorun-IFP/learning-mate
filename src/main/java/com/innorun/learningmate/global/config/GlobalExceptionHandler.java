9c462d176c80ce6be5e039d43ceb086ed9f1eceb
gustavors88 <gustavors88@gmail.com>
2026-08-21 12:13:21 +0900

    feat(validation): add Bean Validation exception handling

--- src/main/java/com/innorun/learningmate/global/config/GlobalExceptionHandler.java
+++ src/main/java/com/innorun/learningmate/global/config/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.innorun.learningmate.global.config;
 
 import com.innorun.learningmate.global.exception.ServiceException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,22 @@ public class GlobalExceptionHandler {
                     .status(ex.getStatus())
                     .body(ex.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst() // gets first error (if it exists, optional)
+                .map(fieldError -> fieldError.getDefaultMessage()) //utilizes message of the error
+                .orElse("입력 값이 올바르지 않습니다.");  // default error message
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
 }
+
+
+
+
+
+
+
 
