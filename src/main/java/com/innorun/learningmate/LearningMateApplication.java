fe31b1fcff4cad7a9fee7a2dfc41124aca7e39b9
Sooyoung Yoon <ayoooo240@gmail.com>
2026-08-24 19:36:04 +0900

    refactor(config): JwtProperties를 명시적으로 등록, UserDetailsService 자동 설정 제외

--- src/main/java/com/innorun/learningmate/LearningMateApplication.java
+++ src/main/java/com/innorun/learningmate/LearningMateApplication.java
@@ -1,13 +1,15 @@
 package com.innorun.learningmate;
 
+import com.innorun.learningmate.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@ConfigurationPropertiesScan
-@SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class LearningMateApplication {
 
     public static void main(String[] args) {
