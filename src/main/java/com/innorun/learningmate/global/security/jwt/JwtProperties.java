7caebfdf5926d6932288b5009b525e0781eb0b61
Sooyoung Yoon <ayoooo240@gmail.com>
2026-08-24 14:11:49 +0900

    feat(jwt): JwtProperties 구성

---
+++ src/main/java/com/innorun/learningmate/global/security/jwt/JwtProperties.java
@@ -0,0 +1,17 @@
+package com.innorun.learningmate.global.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        String secret,
+        Expire expire
+) {
+    public record Expire(
+            Duration access,
+            Duration refresh
+    ) {
+    }
+}
